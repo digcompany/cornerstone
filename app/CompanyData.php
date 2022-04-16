145754aad2a3c86066e8b303cd6ac543612a7534
inmanturbo <inman.turbo@gmail.com>
2022-04-16 12:36:40 -0400

    blank address

--- app/CompanyData.php
+++ app/CompanyData.php
@@ -42,7 +42,8 @@ class CompanyData extends Data implements Company
 
     public function cityStateZip(): string
     {
-        if(!isset($this->address->city) || !isset($this->address->state) || !isset($this->address->zip)) {
+        if(!isset($this->address->city) || !isset($this->address->state) || !isset($this->address->zip) ||
+            strlen($this->address->city) < 1 || strlen($this->address->state) < 1 || strlen($this->address->zip) < 1) {
             return '';
         }
         return $this->address->city . ', ' . $this->address->state . ' ' . $this->address->zip;
